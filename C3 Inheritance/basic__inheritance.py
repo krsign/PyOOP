f2f254bfe1368f5f94fdc8ab6508e08de7f93023
KRISHNA KUMAR <60271340+krsign@users.noreply.github.com>
2020-03-26 23:18:15 +0530

    Add files via upload

--- C3 Inheritance/basic__inheritance.py
+++ C3 Inheritance/basic__inheritance.py
@@ -7,3 +7,23 @@ class Contact:
         self.email = email
         Contact.all_contacts.append(self)
         
+class Supplier(Contact):
+    def order(self, order):
+        print('If this were real system we could send' '{} order to {}'.format(order, self.name))
+        
+        
+c = Contact('Git Hub', 'github@gmail.com')
+s = Supplier('Sup Plier', 'supplier@gmail.com')
+
+print(c.name, c.email, s.name, s.email)
+
+
+c.all_contacts
+print(c.all_contacts)
+
+# contact class will not able to access the value of subclass
+#c.order('Ineed pliers')
+
+s.order(' I need plier')
+
+
